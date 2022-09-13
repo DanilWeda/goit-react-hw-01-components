96162f3723ff87ca30d1c676f6fa8a8a62413834
DanilWeda <draganevr@gmail.com>
2022-09-13 10:32:10 +0300

    less 1: transactions

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import Statistics from './components/Statistics';
 import Profile from './components/Profile'
 import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 import user from './user.json';
 import data from './data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 const App = () => {
 	const [userData, setUserData] = useState(null)
@@ -23,6 +25,7 @@ const App = () => {
 			<Profile {...user} />
 			<Statistics stats={data} />
 			<FriendList friends={friends} />
+			<TransactionHistory items={transactions} />
 		</div>
 	)
 }
